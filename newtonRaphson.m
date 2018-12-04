fa0553c2c370ac00dd33892d1b16991f3181feea
bdosch3 <45244281+bdosch3@users.noreply.github.com>
2018-12-04 15:15:35 -0800

    Add files via upload

--- newtonRaphson.m
+++ newtonRaphson.m
@@ -1,9 +1,12 @@
-function [x_new] = newtonRaphson(J, f_x, x_old)
+function x_new = newtonRaphson(J, f_x, x_old)
 %newtonRaphson performs newton raphson
 %   input: ...
 %   output: ...
 
-dx = -1*inv(J)*x_old;
-x_new = x_old + dx;
+invJacobian = inv(J);
+dx = invJacobian * f_x;
+d_x = -1.*dx;
+%dx = (-1).*(inv(J))*(x_old);
+x_new = x_old + d_x;
 end
 
