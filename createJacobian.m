84b8ad0b281a37506681bc118f02866d91a36038
anasshafawi <45244820+anasshafawi@users.noreply.github.com>
2018-12-04 17:25:30 -0800

    Add files via upload

--- createJacobian.m
+++ createJacobian.m
@@ -1,4 +1,4 @@
-function [J_new] = createJacobian(x, Y, N, m, PV, PQ, Vswing, thetaSwing)
+function [J_new] = createJacobian(x, Y, N, m, PV, f_comp, Vswing, thetaSwing)
 %createJacovian Creates the Jacobian matrix
 %   inputs:
 %              x: column vector of unknowns theta(2:N) and V(m+1:N) (
@@ -18,12 +18,12 @@ V      = [Vswing; PV(m : length(PV)); x(N : length(x))];
 %theta values are the first 2:N entries in x
 theta  =  [thetaSwing; x(1 : (N - 1))];
 %P values from PV will be positive injections into the system (ie a gen.)
-pGen   = PV(1 : (m - 1));
+%pGen   = PV(1 : (m - 1));
 %P values from PQ will be negative injections into the system (ie a load)
-pLoad  = PQ(1 : (N - 1));
+pComp  = f_comp(1 : (N - 1));
 %Q values taken from the "bottom" of PQ will be negative injections
 %positive Q injections for the generators will be explicit equations
-qLoad  = PQ(N : length(PQ));
+qLoad  = computeQ(Y, N, m, theta, V);
      
 %initiate J_new (new Jacobian square matrix)
 %size of J = 2(N-1) by 2(N-1)
@@ -46,27 +46,27 @@ for k = 1:(2*N-m-1)
             end
         % at J_21    
         elseif j <= (N-1) && k > (N-1)
-            if j ~= k
+            if j ~= (k-N+1)
                 J_new(k,j) = -V(k-N+1+m)*V(j+1)*((real(Y(k-N+m,j+1)))*(cos(theta(k-N+m)-theta(j+1)))... 
                             + (imag(Y(k-N+m,j+1))*(sin(theta(k-N+m)-theta(j+1)))));
             else
-                J_new(k,j) = pLoad(k-N+m+1)-(V(k-N+m)^2)*(real(Y(k-N+m,j+1)));
+                J_new(k,j) = pComp(k-N+m)-(V(k-N+m+1)^2)*(real(Y(k-N+m+1,j+1)));
             end
         % at J_12
         elseif j > (N-1) && k <= (N-1)
-            if j ~= k
+            if (j-N+1) ~= k
                 J_new(k,j) = V(k+1)*(real(Y(k+1,j-N+m))*cos(theta(k+1)-theta(j-N+m))... 
                             + (imag(Y(k+1,j-N+m)))*sin(theta(k+1)-theta(j-N+m)));
             else
-                J_new(k,j) = (pLoad(k)/V(k+1)) + V(k+1)*(real(Y(k+1,j-N+m)));
+                J_new(k,j) = (pComp(k)/V(k+1)) + V(k+1)*(real(Y(k+1,j-N+m)));
             end
         % at J_22
         else
             if j ~= k
-                J_new(k,j) = V(k-N+m)*((real(Y(k-N+m,j-N+m)))*(sin(theta(k-N+m)-theta(j-N+m)))... 
-                            - (imag(Y(k-N+m,j-N+m)))*(cos(theta(k-N+m)-theta(j-N+m))));
+                J_new(k,j) = V(k-N+m+1)*((real(Y(k-N+m+1,j-N+m+1)))*(sin(theta(k-N+m+1)-theta(j-N+m+1)))... 
+                            - (imag(Y(k-N+m+1,j-N+m+1)))*(cos(theta(k-N+m+1)-theta(j-N+m+1))));
             else
-                J_new(k,j) = (qLoad(k-N+m+1)/V(k-N+m))-V(k-N+m)*(real(Y(k-N+m,j-N+m)));
+                J_new(k,j) = (qLoad(k-N+m)/V(k-N+m+1))-V(k-N+m+1)*(real(Y(k-N+m+1,j-N+m+1)));
             end
         end
     end
